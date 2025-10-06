1ea85c03879d7e27c3c1337006e68af296d3fa80
Cyber-Rebel <np103177@gmail.com>
2025-10-06 23:08:41 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+      <canvas className='w-full h-full  border-dashed border-gray-300 border-8 text-black'>ddskflsdkfklkld</canvas>
+    </div>
+  )
+}
+
+export default App
